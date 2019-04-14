bb4828c99cdc3a7508255d43d971242e6b7b8d5a
luminji <luminji@hotmail.com>
2019-04-14 10:48:25 +0800

    提交SampleTest01

---
+++ src/com/zuikc/se/SampleTester01.java
@@ -0,0 +1,12 @@
+package com.zuikc.se;
+
+/**
+ * @ClassName SampleTester01
+ * @Description 我们提供咨询和培训服务，关于本文有任何困惑，请关注并联系“码农星球”
+ * @Author 码农星球
+ **/
+public class SampleTester01 {
+    public static void main(String[] args){
+        System.out.println("hell cat~~");
+    }
+}
